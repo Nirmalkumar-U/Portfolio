c09108211b44ff2323e0c36a632664330eac3123
Nirmalkumar-U <unirmal56@gmail.com>
2025-10-08 17:57:31 +0530

    include

--- portfolio/vite.config.ts
+++ portfolio/vite.config.ts
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react'
 //import { ghPages } from 'vite-plugin-gh-pages'; 
 
 export default defineConfig({
-  base: '/Portfolio/',
-  plugins: [
-    react(),
-   // ghPages()
-  ]
+    base: '/Portfolio/',
+    assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.svg'],
+    plugins: [
+        react(),
+        // ghPages()
+    ]
 });
